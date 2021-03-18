d9e6f9c61c4bfb5660351696197627c4eb1fab21
RickWazowski98 <vycheslavsaevskiy98@gmail.com>
2021-03-18 15:54:30 +0200

    init2

--- sh_th-condos.sh
+++ sh_th-condos.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 . "venv/bin/activate"
-#python get_urls_by_each_region_th_condo-1.py
-#python get_urls_by_each_region_th_condo-2.py
+python get_urls_by_each_region_th_condo-1.py
+python get_urls_by_each_region_th_condo-2.py
 python args_dt_dotproperty-1.py -tn "Thailand condominiums"
-#python args_dt_load_to_sheet.py -tn "Thailand condominiums"
-#python args_dt_dotproperty-2.py -tn "Thailand condominiums"
-#python args_dt_dotproperty-mix.py -tn "Thailand condominiums"
+python args_dt_load_to_sheet.py -tn "Thailand condominiums"
+python args_dt_dotproperty-2.py -tn "Thailand condominiums"
+python args_dt_dotproperty-mix.py -tn "Thailand condominiums"
